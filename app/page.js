e313adde7c62fa1d25ae4e940d766180f697f81e
tihmeh <timcostello2020@gmail.com>
2025-12-14 11:26:20 -0500

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Red Apple Portfolio</h1>
+      <p>Initial deployment test — if you see this, the site works.</p>
+    </div>
+  );
+}
