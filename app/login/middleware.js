2b5f7938649d79b0ffe67381abda1006e183c209
tihmeh <timcostello2020@gmail.com>
2025-12-14 12:08:10 -0500

    Update middleware.js

--- app/login/middleware.js
+++ app/login/middleware.js
@@ -2,14 +2,12 @@ import { NextResponse } from "next/server";
 
 export function middleware(req) {
   const authorized = req.cookies.get("authorized")?.value === "yes";
-  const url = req.nextUrl.pathname;
+  const path = req.nextUrl.pathname;
 
-  // Allow login page always
-  if (url.startsWith("/login")) {
+  if (path.startsWith("/login")) {
     return NextResponse.next();
   }
 
-  // Protect all other pages
   if (!authorized) {
     return NextResponse.redirect(new URL("/login", req.url));
   }
